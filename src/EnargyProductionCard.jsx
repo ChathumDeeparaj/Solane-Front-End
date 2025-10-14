5b13b63ebb8640cf4b223123c7d3427991aab392
chathum <deeparajchathum@gmail.com>
2025-10-15 00:35:36 +0530

    update SloaEenragyProductionCard with Anomaly tag

--- src/EnargyProductionCard.jsx
+++ src/EnargyProductionCard.jsx
@@ -1,13 +1,35 @@
 const EnargyProductionCard = (props) => {
-    
+
+
+    // if (props.hasAnomaly) {
+    //     return (
+    //         <div className="relative border border-red-500 rounded-lg ">
+    //         <div className="flex flex-col items-center gap-2 p-6 pb-2">
+    //             <span className="block text-gray-600 text-sm font-medium">{props.day}</span>
+    //             <span className="block text-xs text-gray-500">{props.date}</span>
+    //         </div>
+    //         <div className="p-6 pt-2 flex flex-col items-center">
+    //             <span className="block mb-1 text-3xl font-bold text-red-600">{props.production}</span>
+    //             <span className="block text-sm font-medium text-red-500">KWh</span>
+    //         </div>
+    //     </div>
+    //     )
+    // }
+
+
     return (
-        <div className="relative border border-gray-500 rounded-lg ">
+        <div className={`relative border ${props.hasAnomaly ? "border-red-500" : "border-gray-300"} rounded-lg `}>
+            {
+                props.hasAnomaly && (
+                    <div className="absolute top-0 right-0 bg-red-500 text-white px-2 py-2 text-sm rounded-bl-lg">Anomaly</div>
+                )
+            }
             <div className="flex flex-col items-center gap-2 p-6 pb-2">
                 <span className="block text-gray-600 text-sm font-medium">{props.day}</span>
                 <span className="block text-xs text-gray-500">{props.date}</span>
             </div>
             <div className="p-6 pt-2 flex flex-col items-center">
-                <span className="block mb-1 text-3xl font-bold text-blue-600">{props.production}</span>
+                <span className={props.hasAnomaly ? "block mb-1 text-3xl font-bold text-red-600" :"block mb-1 text-3xl font-bold text-blue-600"}>{props.production}</span>
                 <span className="block text-sm font-medium text-gray-500">KWh</span>
             </div>
         </div>
--- src/components/SolarEnergyProduction/SolarEnergyProduction.jsx
+++ src/components/SolarEnergyProduction/SolarEnergyProduction.jsx
@@ -1,21 +1,35 @@
 import EnargyProductionCard from "@/EnargyProductionCard";
 
 const SolarEnergyProduction = () => {
+    const enargyProductiondata = [
+        { day: "Mon", date: "Aug 18", production: 34.1 , hasAnomaly:false},
+        { day: "Tue", date: "Aug 19", production: 32.1 , hasAnomaly:true},
+        { day: "Wed", date: "Aug 20", production: 36.9 , hasAnomaly:false},
+        { day: "Thu", date: "Aug 21", production: 30.9 , hasAnomaly:false},
+        { day: "Fri", date: "Aug 22", production: 34.1 , hasAnomaly:false},
+        { day: "Sat", date: "Aug 23", production: 20.1 , hasAnomaly:true},
+        { day: "Sun", date: "Aug 24", production: 35.1 , hasAnomaly:false},
+    ];
   return (
-    <section className="px-12 py-6 font-['Inter'] py-6 ">
+    <section className="px-12 py-6 font-[Inter] py-6 ">
         <div classsname="mb-6">
-            <h2 className="mb-2.text-2xl.font-bold.text-gray-900">Solar Energy Production</h2>
+            <h2 className="mb-2 text-2xl font-bold 	font-weigh-500 text-gray-900">Solar Energy Production</h2>
             <p className="text-gray-600">Daily enargy output for the past 7 days</p>
         </div>
         <br></br>
         <div className="grid grid-cols-7 gap-4">
-            <EnargyProductionCard day="mon" date="Aug 18" production={34.1} />
-            <EnargyProductionCard day="tue" date="Aug 19" production={32.1}/>
-            <EnargyProductionCard day="wed" date="Aug 20" production={36.9}/>
-            <EnargyProductionCard day="thu" date="Aug 21" production={30.9}/>
-            <EnargyProductionCard day="fri" date="Aug 22" production={34.1}/>
-            <EnargyProductionCard day="sat" date="Aug 23" production={20.1}/>
-            <EnargyProductionCard day="sun" date="Aug 24" production={35.1}/>
+            {enargyProductiondata.map((el) => {
+                return(
+                    <EnargyProductionCard 
+                    key={el.date} 
+                    day={el.day} 
+                    date={el.date} 
+                    production={el.production}
+                    hasAnomaly={el.hasAnomaly}
+                    />
+                    
+                );
+            })} 
         </div>
     </section>
 
