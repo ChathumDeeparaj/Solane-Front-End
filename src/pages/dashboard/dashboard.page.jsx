f59a5ad6f8373ca1cebecfeca5193139bef34ad9
chathum <deeparajchathum@gmail.com>
2025-12-09 23:59:53 +0530

    add chart to the dashboard

--- src/pages/dashboard/dashboard.page.jsx
+++ src/pages/dashboard/dashboard.page.jsx
@@ -1,9 +1,37 @@
-import Navigation from "@/components/Navigation/Navigation";
+import { useGetEnergyGenerationRecordsBySolarUnitQuery } from "@/lib/redux/query";
+import DataCard from "./components/DataCard";
+import DataChart from "./components/DataChart";
 
 const DashboardPage = () => {
+  const { data, isLoading, isError, error } =
+    useGetEnergyGenerationRecordsBySolarUnitQuery({
+      id: "68f4f2cef076449e2049b9c1",
+      groupBy: "date",
+    });
+
+  if (isError || !data) return null;
+
   return (
-    <main className="px-12">
+    <main className="mt-4">
       <h1 className="text-4xl font-bold text-foreground">Alice's House</h1>
+      <p className="text-gray-600 mt-2">Welcome back to your Solar Energy Production Dashboard</p>
+      <div className="mt-8">
+        <DataCard
+          data={data}
+          isLoading={isLoading}
+          isError={isError}
+          error={error}
+          title="Last 7 Days Energy Production"
+        />
+      </div>
+      <div className="mt-8">
+        <DataChart
+          data={data}
+          isLoading={isLoading}
+          isError={isError}
+          error={error}
+        />
+      </div>
     </main>
   );
 };
