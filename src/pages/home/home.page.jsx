08b6da6b42b3aad2a6a9b49794d44802a37b98b9
chathum <deeparajchathum@gmail.com>
2025-12-13 21:12:45 +0530

    updated with anomaly detection

--- src/pages/home/home.page.jsx
+++ src/pages/home/home.page.jsx
@@ -1,5 +1,5 @@
 import HeroSection from "@/pages/home/components/HeroSection/HeroSection";
-import SolarEnergyProduction from "@/pages/home/components/SolarEnergyProduction";
+// import SolarEnergyProduction from "@/pages/home/components/SolarEnergyProduction";
 import HomepageBody1 from "@/pages/home/components/HomepageBody/HomepageBody1";
 import HomepageBody2 from "@/pages/home/components/HomepageBody/HomepageBody2";
 import HomepageBody3 from "@/pages/home/components/HomepageBody/HomepageBody3";
@@ -8,7 +8,7 @@ const HomePage = () => {
   return (
            <main className="space-y-6 px-2 sm:px-6 lg:px-12">
              <HeroSection />
-             <SolarEnergyProduction />
+             {/* <SolarEnergyProduction /> */}
              <HomepageBody1 />
              <br />
              <HomepageBody2 />
