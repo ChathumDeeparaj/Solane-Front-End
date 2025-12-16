0ad7c5eb5b19ce565d6d68eb2d72daaa6970f8b7
chathum <deeparajchathum@gmail.com>
2025-12-16 12:55:16 +0530

    update

--- vite.config.js
+++ vite.config.js
@@ -1,35 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-import tailwindcss from '@tailwindcss/vite'
-import path from "path";
-
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
+  plugins: [react()],
   build: {
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes('node_modules')) {
-            if (id.includes('react') || id.includes('react-dom') || id.includes('react-router')) {
-              return 'react-vendor';
-            }
-            if (id.includes('recharts')) {
-              return 'recharts';
-            }
-            if (id.includes('@radix-ui') || id.includes('lucide-react')) {
-              return 'ui-vendor';
-            }
-            return 'vendor';
-          }
-        },
-      },
-    },
+    outDir: 'dist',
+    sourcemap: false,
   },
-});
+  base: './',
+})
